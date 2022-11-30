67f90cc8994c100aaced5cd40d765b566f5fd0fb
UK <usman.khalid@hotelkeyapp.com>
2022-11-30 17:03:11 +0500

    fetch device by ID

--- src/main/java/com/resources/AppleStoreResource.java
+++ src/main/java/com/resources/AppleStoreResource.java
@@ -4,17 +4,27 @@ import com.services.AppleStoreService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/inventory")
 public class AppleStoreResource {
 
+    AppleStoreService appleStoreService = new AppleStoreService();
+
     @GET
     @Path("/list")
     public Response listAll()
     {
-        AppleStoreService li = new AppleStoreService();
-        String str = li.list();
+        String str = appleStoreService.fetchAll();
+        return Response.ok(str).build();
+    }
+
+    @GET
+    @Path("/{inventory_id}")
+    public Response fetchById(@PathParam("inventory_id")Integer id)
+    {
+        String str = appleStoreService.fetchById(id);
         return Response.ok(str).build();
     }
 }
